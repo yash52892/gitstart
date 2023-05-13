de28f58eff66f81711a282a8e486845fab24f4d2
yash52892 <yashwanth.sandcube@gmail.com>
2023-05-13 15:24:37 +0530

    Edit button is added

--- Git/index.js
+++ Git/index.js
@@ -1,1 +1,10 @@
-console.log("time");
+var itemList = document.getElementById('items');
+itemList.addEventListener('click', removeItem);
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+      }
+    }
+  }
